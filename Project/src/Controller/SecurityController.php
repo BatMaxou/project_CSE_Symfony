138cd08c82c7f12f22c3d82233ce3979eafeca85
Maxime Batista <maximebatista.lycee@gmail.com>
2023-03-16 15:26:50 +0100

    twig ajust && var css

--- Project/src/Controller/SecurityController.php
+++ Project/src/Controller/SecurityController.php
@@ -2,9 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Subscriber;
-use App\Form\UserResponseType\SubscriberType;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,27 +10,18 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
         $path = [['Accueil', 'home'], ['Login', 'app_login']];
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        $sub = new Subscriber();
-        $formSub = $this->createForm(SubscriberType::class, $sub);
-        $formSub->handleRequest($request);
-
-        return $this->render('security/login.html.twig', [
+        return $this->render('security/index.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
             'path' => $path,
-            'formSub' => $formSub->createView(),
         ]);
     }
 
