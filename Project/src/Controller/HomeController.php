39c26a6acfb9fd704d7395d949d05d181e4c405e
Antony <sacha02200@gmail.com>
2023-03-16 09:41:55 +0100

    page contact with form and without captcha

--- Project/src/Controller/HomeController.php
+++ Project/src/Controller/HomeController.php
@@ -4,16 +4,19 @@ namespace App\Controller;
 
 use Exception;
 use App\Entity\Survey;
+use App\Entity\Contact;
 use App\Entity\Subscriber;
 use App\Entity\UserResponse;
 use App\Repository\SurveyRepository;
+use App\Form\ContactType\ContactType;
 use App\Repository\CkeditorRepository;
 use App\Repository\ResponseRepository;
 use App\Repository\TicketingRepository;
+use App\Repository\SubscriberRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\PartnershipRepository;
-use App\Repository\ImageTicketingRepository;
 use App\Entity\Response as ResponseSurvey;
+use App\Repository\ImageTicketingRepository;
 use App\Form\UserResponseType\SubscriberType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -35,6 +38,7 @@ class HomeController extends AbstractController
 
         // get the question active of the survey
         $questionActive = $surveyRepo->findQuestionActive();
+
         // get response associated at the question of the survey
         $responseQuestion = $responseRepo->findResponseById($questionActive->getIdSurvey());
 
@@ -84,6 +88,7 @@ class HomeController extends AbstractController
 
         // get the question active of the survey
         $questionActive = $surveyRepo->findQuestionActive();
+
         // get response associated at the question of the survey
         $responseQuestion = $responseRepo->findResponseById($questionActive->getIdSurvey());
 
@@ -133,6 +138,7 @@ class HomeController extends AbstractController
 
         // get the question active of the survey
         $questionActive = $surveyRepo->findQuestionActive();
+
         // get response associated at the question of the survey
         $responseQuestion = $responseRepo->findResponseById($questionActive->getIdSurvey());
 
@@ -225,6 +231,7 @@ class HomeController extends AbstractController
             }
         }
 
+        // si $offer retourne quelque chose et que l'id est un numérique alors on render sinon redirect
         if (($offer != NULL) and (is_numeric($id))) {
             // get image associated at the id in the url of the ticketing
             $imgOffer = $imgTicketingRepo->findImageTicketing($id);
@@ -245,12 +252,80 @@ class HomeController extends AbstractController
     }
 
     #[Route(path: '/contact', name: 'contact')]
-    public function contact(): Response
+    public function contact(PartnershipRepository $partnershipRepo, Request $request, SurveyRepository $surveyRepo, ResponseRepository $responseRepo, EntityManagerInterface $manager, SubscriberRepository $subscriberRepo): Response
     {
         $path = [['Accueil', 'home'], ['Contact', 'contact']];
 
-        return $this->render('page.html.twig', [
+        // get 3 random image from database
+        $imgPartner = $partnershipRepo->imagePartner();
+
+        // get the question active of the survey
+        $questionActive = $surveyRepo->findQuestionActive();
+
+        // get response associated at the question of the survey
+        $responseQuestion = $responseRepo->findResponseById($questionActive->getIdSurvey());
+
+        $userResponse = new UserResponse();
+        $form = $this->createForm(UserResponseType::class, $userResponse);
+        $form->handleRequest($request);
+
+        $sub = new Subscriber();
+        $formSub = $this->createForm(SubscriberType::class, $sub);
+        $formSub->handleRequest($request);
+
+        $contact = new Contact();
+        $formContact = $this->createForm(ContactType::class, $contact);
+        $formContact->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            try {
+                // get id of the respons by a search name for set response of the create UserResponse
+                $response = $responseRepo->findIdResponseOfName($request->get("radio_response"));
+
+                $userResponse->setResponse($response);
+                $manager->persist($userResponse);
+                $manager->flush();
+
+                $this->addFlash('success', 'Réponse enregistrée, merci de votre participation !');
+            } catch (\Throwable $th) {
+                $this->addFlash('error', 'Une erreur imprévu est survenu, veillez recharger la puis réessayer.');
+            }
+        }
+
+        if ($formContact->isSubmitted() && $formContact->isValid()) {
+            try {
+                if (!empty($_POST['consent']) && $_POST['consent'] === 'on') {
+                    if (preg_match('/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i', $formContact->getData()->getEmailContact())) {
+                        if ($subscriberRepo->countByMail($formContact->getData()->getEmailContact()) === 0) {
+                            $sub = new Subscriber();
+                            $sub->setEmailSubscriber($formContact->getData()->getEmailContact());
+                            $sub->setConsentSubscriber(1);
+                            $subscriberRepo->save($sub, true);
+                        }
+                    }
+                }
+                
+                $contact = $formContact->getData();
+                
+                $manager->persist($contact);
+                $manager->flush();
+                
+                $this->addFlash('success', 'Votre message a bien été envoyé !');
+                
+                return $this->redirectToRoute('contact');
+            } catch (\Throwable $th) {
+                $this->addFlash('error', 'Une erreur imprévu est survenu, veillez recharger la puis réessayer.');
+            }
+        }
+
+        return $this->render('contact/contact.html.twig', [
             'path' => $path,
+            'image' => $imgPartner,
+            'question' => $questionActive,
+            'response' => $responseQuestion,
+            'form' => $form->createView(),
+            'formSub' => $formSub->createView(),
+            'formContact' => $formContact->createView(),
         ]);
     }
 }
--- Project/src/Entity/Contact.php
+++ Project/src/Entity/Contact.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\ContactRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ContactRepository::class)]
 class Contact
@@ -15,16 +16,53 @@ class Contact
     private ?int $idContact = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\Length(
+        min: 2,
+        max: 50,
+        minMessage: 'Vous devez écrire au moins 2 caractères pour le prénom.',
+        maxMessage: 'Vous devez écrire moins de 50 caractères pour le prénom.',
+    )]
+    #[Assert\NotBlank(
+        message: 'Le prénom ne peut pas être vide, veuillez le renseigner.',
+    )]
     private ?string $nameContact = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\Length(
+        min: 2,
+        max: 50,
+        minMessage: 'Vous devez écrire au moins 2 caractères pour le nom.',
+        maxMessage: 'Vous devez écrire moins de 50 caractères pour le nom.',
+    )]
+    #[Assert\NotBlank(
+        message: 'Le nom ne peut pas être vide, veuillez le renseigner.',
+    )]
     private ?string $firstnameContact = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\Email(
+        message: 'Adresse mail invalid.',
+    )]
+    #[Assert\NotBlank(
+        message: 'L\'adresse mail ne peut pas être vide, veuillez le renseigner.',
+    )]
     private ?string $emailContact = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Assert\Length(
+        min: 2,
+        minMessage: 'Vous devez écrire au moins 2 caractères pour le message.',
+    )]
+    #[Assert\NotBlank(
+        message: 'Le message ne peut pas être vide, veuillez le renseigner.',
+    )]
     private ?string $messageContact = null;
+
+    #[ORM\Column(nullable: true)]
+    #[Assert\NotBlank(
+        message: 'Vous êtes obligé d\'accepter que le site utilise vos informations renseignés afin de prendre contact avec vous.',
+    )]
+    private ?bool $consentContact = null;
 
     public function getIdContact(): ?int
     {
@@ -75,6 +113,18 @@ class Contact
     public function setMessageContact(string $messageContact): self
     {
         $this->messageContact = $messageContact;
+
+        return $this;
+    }
+
+    public function isConsentContact(): ?bool
+    {
+        return $this->consentContact;
+    }
+
+    public function setConsentContact(?bool $consentContact): self
+    {
+        $this->consentContact = $consentContact;
 
         return $this;
     }
