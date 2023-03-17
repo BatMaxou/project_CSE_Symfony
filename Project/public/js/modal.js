ba192c6953948d4f933b9f23ae03be672a446e4c
Antony <sacha02200@gmail.com>
2023-03-17 09:10:07 +0100

    fix bug modal

--- Project/public/js/modal.js
+++ Project/public/js/modal.js
@@ -1,26 +1,26 @@
-const modal = document.querySelector('.modal');
+const modalBackground = document.querySelector('.modal-background');
 const modalOpen = document.querySelector('.modal-open');
 const modalClose = document.querySelector('.modal-close');
-const modalContent = document.querySelector('.modal-content');
+const modal = document.querySelector('.modal');
 const btnClose = document.querySelector('.btnClose');
 
 // création d'une timeline et paused sur true pour pas que ça se lance des le load de la page
 const timeline = gsap.timeline({ paused: true });
 
 // -50 pour le faire apparaitre de haut en bah centré progressivement de 50 px
-timeline.from(modalContent, { y: -50, duration: 0.5 });
+timeline.from(modal, { y: -100, duration: 0.5 });
 
 // on joue la timeline
 timeline.play();
 
 function openModal() {
-    modal.style.display = 'block';
+    modalBackground.style.display = 'block';
     // réinitialisé la timeline
     timeline.restart();
 }
 
 function closeModal() {
-    modal.style.display = 'none';
+    modalBackground.style.display = 'none';
 }
 
 modalOpen.addEventListener('click', openModal);
@@ -28,9 +28,9 @@ modalClose.addEventListener('click', closeModal);
 btnClose.addEventListener('click', closeModal);
 
 // pour fermer lorsqu'on clique en dehors du modal
-modal.addEventListener('click', (event) => {
+modalBackground.addEventListener('click', (event) => {
     // si l'élèment sur lequel l'utilisateur a cliqué === à l'lélèment qu'on a sélectionné 
-    if (event.target === modal) {
+    if (event.target === modalBackground) {
         closeModal();
     }
 });
