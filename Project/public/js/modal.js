c347d56d609415d3765be323a94a96a50a10156d
Maxime Batista <maximebatista.lycee@gmail.com>
2023-03-28 16:09:56 +0200

    modal.js ajust && css

--- Project/public/js/modal.js
+++ Project/public/js/modal.js
@@ -2,28 +2,38 @@ const modalBackground = document.querySelector('.modal-background');
 const modalOpen = document.querySelector('.modal-open');
 const modalClose = document.querySelector('.modal-close');
 const modal = document.querySelector('.modal');
+const modalHeader = modal.querySelector('.modal-header');
+const modalBody = modal.querySelector('.modal-body');
+const modalFooter = modal.querySelector('.modal-footer');
 const btnClose = document.querySelector('.btnClose');
+
+// gestion hauteur du modal
+let modalHeaderHeight = 0
+let modalFooterHeight = 0
+
+const handleHeightChange = () => {
+    modalBody.style.height = modal.clientHeight - modalHeader.clientHeight - modalFooter.clientHeight - 40 + 'px'
+}
+
+window.addEventListener("resize", handleHeightChange)
 
 // création d'une timeline et paused sur true pour pas que ça se lance des le load de la page
 const timeline = gsap.timeline({ paused: true });
 
-// -50 pour le faire apparaitre de haut en bah centré progressivement de 50 px
-timeline.from(modal, { y: -100, duration: 0.5 });
-
-// on joue la timeline
-timeline.play();
+// -25 pour le faire apparaitre de haut en bah centré progressivement de 50 px
+timeline.from(modal, { y: '-25px', duration: 0.5 })
 
 function openModal() {
     modalBackground.style.display = 'block';
-    // réinitialisé la timeline
-    timeline.restart();
+    handleHeightChange()
+
+    // on joue la timeline
+    timeline.restart()
 }
 
 function closeModal() {
     modalBackground.style.display = 'none';
 }
-
-console.log(modalOpen);
 
 modalOpen.addEventListener('click', openModal);
 modalClose.addEventListener('click', closeModal);
