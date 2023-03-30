887ae75dc93e0a6dd6acf891344592d30362da47
Antony <sacha02200@gmail.com>
2023-03-30 13:20:57 +0200

    add public fonction in survey repository

--- Project/src/Repository/SurveyRepository.php
+++ Project/src/Repository/SurveyRepository.php
@@ -3,8 +3,9 @@
 namespace App\Repository;
 
 use App\Entity\Survey;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query\Expr\Join;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @extends ServiceEntityRepository<Survey>
@@ -49,32 +50,61 @@ class SurveyRepository extends ServiceEntityRepository
             ->setParameter('active', $active)
             ->setMaxResults(1)
             ->getQuery()
-            ->getOneOrNullResult()
-        ;
+            ->getOneOrNullResult();
     }
 
-//    /**
-//     * @return Survey[] Returns an array of Survey objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('s.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    // retourne le nombre total de réponse associé à un survey
+    public function totalResponseBySurvey(): array
+    {
+        $qb = $this->createQueryBuilder('survey');
 
-//    public function findOneBySomeField($value): ?Survey
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+        $qb->select('survey.question', 'survey.datetime', 'COUNT(user_response.id) AS total_responses')
+            ->innerJoin('App\Entity\Response', 'response', 'WITH', 'response.survey = survey.id')
+            ->innerJoin('App\Entity\UserResponse', 'user_response', 'WITH', 'user_response.response = response.id')
+            ->groupBy('survey.question');
+
+        return $qb->getQuery()->getResult();
+    }
+
+    // retourne le nombre total de réponse associé à une question associé à un survey
+    public function totalResponseByQuestion(): array
+    {
+
+        $qb = $this->createQueryBuilder('survey');
+
+        $qb->select('survey.question', 'response.text', 'COUNT(user_response.id) AS responses')
+            ->innerJoin('App\Entity\Response', 'response', 'WITH', 'response.survey = survey.id')
+            ->innerJoin('App\Entity\UserResponse', 'user_response', 'WITH', 'user_response.response = response.id')
+            ->groupBy('survey.question, response.text');
+
+        return $qb->getQuery()->getResult();
+    }
+
+
+
+
+    //    /**
+    //     * @return Survey[] Returns an array of Survey objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('s')
+    //            ->andWhere('s.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('s.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Survey
+    //    {
+    //        return $this->createQueryBuilder('s')
+    //            ->andWhere('s.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
